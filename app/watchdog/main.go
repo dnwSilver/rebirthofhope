eb3a7c6dd7f8b0fc4de2b1eda7a4cee90ae83d4d
Gavrilov Vadim <gavrilov.v@avtocod.ru>
2024-11-15 15:43:48 +0500

    footage main page

--- app/watchdog/main.go
+++ app/watchdog/main.go
@@ -2,25 +2,43 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"text/template"
+
+	"github.com/go-chi/chi"
 )
 
 func main() {
-	fmt.Println("Hello, Vaaadim. 😉")
-
 	features := os.Getenv("FEATURES")
 
 	if !strings.Contains(features, "WATCH") {
-		fmt.Println("Feature WATCH disabled.")
+		log.Fatal("Feature WATCH disabled.")
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	r := chi.NewRouter()
+	r.Get("/", serveMainPage)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fileServer := http.FileServer(http.Dir("/.output/public/arts/watchdog"))
+	r.Handle("/static/*", http.StripPrefix("/static/", fileServer))
+
+	log.Printf("Server is starting on :%d...\n", 8080)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", 8080), r); err != nil {
+		log.Fatal("Error starting the server: ", err)
+	}
+}
+
+type MainView struct {
+	Images []string
+}
+
+func serveMainPage(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.ParseFiles("./web/templates/main.html"))
+
+	err := tpl.ExecuteTemplate(w, "main.html", &MainView{[]string{"footage-screenshot-4f358472-59f1-47d8-a53f-bf58b0aebaf0.png"}})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
