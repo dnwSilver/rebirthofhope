5cd0aa932976547881754dcca4f1381183047bbf
Александр Колосов <dnwSilver@gmail.com>
2024-11-26 21:28:30 +0500

    🎬 It has begun.

--- app/web/src/helpers/cookies.ts
+++
@@ -1,7 +0,0 @@
-export const getCookie = (name: string) => {
-  if (typeof document !== "undefined") {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts?.pop()?.split(";").shift();
-  }
-};
