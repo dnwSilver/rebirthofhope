3aa202d9c76c7fe836807c3bc039c88f64a551cb
Александр Колосов <dnwSilver@gmail.com>
2024-11-26 21:26:01 +0500

    🎬 It has begun.

--- app/web/src/components/tutorials/commands.ts
+++
@@ -1,7 +0,0 @@
-export const REPO_GO = "cd ~/rebirthofhope;";
-export const REPO_CLONE = (call: string | undefined) => `git clone --branch savior/${call} git@github.com:dnwSilver/rebirthofhope.git;`;
-export const RUN_LINTING = "make verify;";
-export const RUN_DEPLOY = "helmfile --environment production-app apply;";
-export const GIT_STAGE = `git add {{YOUR_FILES}};`;
-export const GIT_COMMIT = 'git commit --message "I did it!";';
-export const GitPush = (call: string | undefined) => `git push origin savior/${call};`;
